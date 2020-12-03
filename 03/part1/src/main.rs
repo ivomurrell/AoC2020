6e252d3ad98ee9df4aa96010bf00941ffd066be9
Ivo Murrell <ivoivo@hey.com>
2020-12-03 08:01:36 +0000

    Submission for day 3 part 1.

---
+++ 03/part1/src/main.rs
@@ -0,0 +1,15 @@
+use anyhow::{self, Context as _};
+use std::fs::read_to_string;
+
+fn main() -> anyhow::Result<()> {
+    let map = read_to_string("../input").context("Failed to open input file")?;
+    let pattern_length = map.lines().next().context("Input was empty")?.len();
+    let tree_encounters = map
+        .lines()
+        .enumerate()
+        // Assume that all lines are the same length
+        .filter(|(i, row)| row.as_bytes()[(i * 3) % pattern_length] == b'#')
+        .count();
+    println!("Would encounter {} trees on the slope!", tree_encounters);
+    Ok(())
+}
