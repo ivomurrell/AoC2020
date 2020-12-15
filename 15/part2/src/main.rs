d924e06caaab97081b4c6a2b4b691824be30d9a2
Ivo Murrell <ivoivo@hey.com>
2020-12-15 10:46:53 +0000

    Submission for day 15 part 2.

---
+++ 15/part2/src/main.rs
@@ -0,0 +1,18 @@
+use std::{collections::HashMap, fs::read_to_string};
+
+fn main() {
+    let mut memory: HashMap<usize, _> = read_to_string("../input")
+        .expect("Failed to open input file")
+        .split(',')
+        .enumerate()
+        .map(|(step, num)| (num.parse().expect("Could not parse number"), step))
+        .collect();
+    let mut next = 0;
+    for step in memory.len()..29999999 {
+        next = match memory.insert(next, step) {
+            Some(last) => step - last,
+            None => 0,
+        }
+    }
+    println!("30000000th number in the memory game is {}!", next);
+}
